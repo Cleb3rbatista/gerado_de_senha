c0a2300ee26ae3b615ff7fde1aa9fd88cf6440a1
Cleb3rbatista <101151617+Cleb3rbatista@users.noreply.github.com>
2022-08-15 23:19:49 -0300

    gerador de senha

---
+++ senhas.py
@@ -0,0 +1,13 @@
+import random
+letra_minuscula ="qwertyuioplkjhgfdsazxcvbnm"
+letra_maiuscula ="QWERTYUIOPLKJHGFDSAZXCVBNM"
+numeros ="0123456789"
+sibolos ="!@#$%¨&*()_+"
+n=0
+while n<5:
+    n+=1
+    misturar=letra_maiuscula+letra_minuscula+sibolos+numeros
+    quatidade_de_caracteres = 20
+    password="".join(random.sample(misturar,quatidade_de_caracteres))
+    print(f"Sua senha é{password}")
+    print("-" * 30)
